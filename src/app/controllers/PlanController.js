f988193b172dc53a30a09daf3c6a077afc36ea24
Edilson <edilson.a.andrade@gmail.com>
2019-10-30 21:06:27 -0300

    update, delete and list plans done

--- src/app/controllers/PlanController.js
+++ src/app/controllers/PlanController.js
@@ -21,7 +21,9 @@ class PlanController {
 
     const { duration, title, price, description } = req.body;
 
-    const existPlan = await Plan.findAll({ where: { duration, price } });
+    const existPlan = await Plan.findOne({
+      where: { duration, price },
+    });
     if (existPlan) {
       return res
         .status(401)
@@ -32,6 +34,58 @@ class PlanController {
     const plan = await Plan.create({ duration, title, price, description });
 
     return res.json(plan);
+  }
+
+  async index(req, res) {
+    const plans = await Plan.findAll();
+    return res.json(plans);
+  }
+
+  async delete(req, res) {
+    const id = req.params.planId;
+
+    const existPlan = await Plan.findByPk(id);
+    if (!existPlan) {
+      return res.status(404).json('Plan selected to delete does not exist');
+    }
+    await existPlan.destroy();
+
+    return res.json();
+  }
+
+  async update(req, res) {
+    const id = req.params.planId;
+    if (!id) {
+      return res.status(401).json('Plan id must be informed');
+    }
+    const schema = Yup.object().shape({
+      title: Yup.string().min(3),
+      duration: Yup.number().min(1),
+      price: Yup.number().min(50),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.json(401).json('Validation failed');
+    }
+
+    const existPlan = await Plan.findByPk(id);
+    if (!existPlan) {
+      return res.status(404).json('Plan selected to delete does not exist');
+    }
+
+    const { title, duration, price, description } = req.body;
+
+    if (title || duration || price || description) {
+      const plan = await existPlan.update({
+        title,
+        duration,
+        price,
+        description,
+      });
+
+      return res.json(plan);
+    }
+    return res.json('No changes were made');
   }
 }
 export default new PlanController();
--- src/routes.js
+++ src/routes.js
@@ -14,4 +14,8 @@ routes.use(auth);
 routes.post('/students', StudentController.store);
 routes.put('/students', StudentController.update);
 routes.post('/plans', PlanController.store);
+routes.get('/plans', PlanController.index);
+routes.put('/plans/:planId', PlanController.update);
+routes.delete('/plans/:planId', PlanController.delete);
+
 export default routes;
